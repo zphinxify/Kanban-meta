4eb1b2e6164f2b3e230edeb82d22b5384c6c7e48
unknown <drawserqzez@gmail.com>
2020-06-16 10:56:46 +0200

    Started moving backend to Razor

---
+++ Client/Models/KanbanContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Client.Models {
+    public class KanbanContext : DbContext {
+        public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) {
+
+        }
+
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Column> Columns { get; set; }
+    }
+}
