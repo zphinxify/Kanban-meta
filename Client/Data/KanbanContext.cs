e6667402964e104e9a88264f20c26d4cf8bdfc75
Zphinxify <benjamiiiin94@gmail.com>
2020-06-16 11:45:22 +0200

    Rebased Context

--- Client/Data/KanbanContext.cs
+++ Client/Data/KanbanContext.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Client.Models;
 
-namespace Client.Data {
-    public class KanbanContext : DbContext {
-        public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) {
 
+namespace Client.Data
+{
+    public class KanbanContext : DbContext
+    {
+        public KanbanContext (
+            DbContextOptions<KanbanContext> options)
+            : base(options)
+        {
         }
 
         public DbSet<Card> Cards { get; set; }
         public DbSet<Column> Columns { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Card>().ToTable("Card");
+            modelBuilder.Entity<Column>().ToTable("Column");
+        }
     }
 }
